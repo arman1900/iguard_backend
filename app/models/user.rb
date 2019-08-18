3f25aae74013773a1eb38153f6eb0995348ddf20
arman1900 <arma23430@gmail.com>
2019-08-18 15:11:25 +0600

    ''

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   #  before_create :confirmation_token
     cattr_reader :current_password
-    has_and_belongs_to_many :securities, :tags, -> { distinct }
+    has_and_belongs_to_many :securities, -> { distinct }
     has_many :notifications
     has_many :camera_settings
     has_secure_password
