5efb7b3aa225c4739553ae5dbbfb435b28e5c8fd
arman1900 <arma23430@gmail.com>
2019-08-18 19:13:39 +0600

    ''

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,12 +7,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   process :tags => ['post_picture']
 
   version :standard do
-    process resize_to_fit: [600, 800]
+    process :resize_to_fill => [100, 150, :north]
   end
 
   version :thumbnail do
-    resize_to_fit(100, 150)
+    resize_to_fit(50, 50)
   end
-  
 
 end
