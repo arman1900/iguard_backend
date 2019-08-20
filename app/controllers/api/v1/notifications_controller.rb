963306a7e93f148d4bc9af0f7978f32a7f0b0772
arman1900 <arma23430@gmail.com>
2019-08-20 14:40:53 +0600

    ''

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -8,14 +8,12 @@ class Api::V1::NotificationsController < ApplicationController
         render json:  notification
     end
     def show_user_cameras
-        content = []
         cam = CameraSetting.where(user_id: params[:user_id])
         cam.each do |a|    
             @id=a.id
             @notification = Notification.where(camera_id: @id)
-            content << @notification
         end
-        render json: {:data=> content, :status=> 200}
+        render json: {:data=> @notification, :status=> 200}
     end
     def create
         cam = CameraSetting.find(params[:camera_id])
